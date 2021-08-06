7f194b355954fd547c1faf0dc9e8bdae517c740a
HTG-YT <htgazurex1212.0001@gmail.com>
2021-08-06 22:54:24 +0800

    init crate

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MPL-2.0
+/*
+ * File: src/lib.rs
+ *
+ * The externc-libm library.
+ *
+ * Author: HTG-YT
+ * Copyright (c) 2021 The Bootmgr Team of the HaruxOS Project
+ */
+
+#![no_std]
