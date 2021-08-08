3f6e0ba2e12f68bef12d22d4561d350cd4b24d0c
HTG-YT <htgazurex1212.0001@gmail.com>
2021-08-08 11:52:44 +0800

    update module import and crate features

--- src/lib.rs
+++ src/lib.rs
@@ -10,227 +10,117 @@
 
 #![no_std]
 
-#[path = "math/acos.rs"]
-pub mod acos;
-#[path = "math/acosf.rs"]
-pub mod acosf;
-#[path = "math/acosh.rs"]
-pub mod acosh;
-#[path = "math/acoshf.rs"]
-pub mod acoshf;
-#[path = "math/asin.rs"]
-pub mod asin;
-#[path = "math/asinf.rs"]
-pub mod asinf;
-#[path = "math/asinh.rs"]
-pub mod asinh;
-#[path = "math/asinhf.rs"]
-pub mod asinhf;
-#[path = "math/atan.rs"]
-pub mod atan;
-#[path = "math/atan2.rs"]
-pub mod atan2;
-#[path = "math/atan2f.rs"]
-pub mod atan2f;
-#[path = "math/atanf.rs"]
-pub mod atanf;
-#[path = "math/atanh.rs"]
-pub mod atanh;
-#[path = "math/atanhf.rs"]
-pub mod atanhf;
-#[path = "math/cbrt.rs"]
-pub mod cbrt;
-#[path = "math/cbrtf.rs"]
-pub mod cbrtf;
-#[path = "math/ceil.rs"]
-pub mod ceil;
-#[path = "math/ceilf.rs"]
-pub mod ceilf;
-#[path = "math/copysign.rs"]
-pub mod copysign;
-#[path = "math/copysignf.rs"]
-pub mod copysignf;
-#[path = "math/cos.rs"]
-pub mod cos;
-#[path = "math/cosf.rs"]
-pub mod cosf;
-#[path = "math/cosh.rs"]
-pub mod cosh;
-#[path = "math/coshf.rs"]
-pub mod coshf;
-#[path = "math/erf.rs"]
-pub mod erf;
-#[path = "math/erfc.rs"]
-pub mod erfc;
-#[path = "math/erfcf.rs"]
-pub mod erfcf;
-#[path = "math/erff.rs"]
-pub mod erff;
-#[path = "math/exp.rs"]
-pub mod exp;
-#[path = "math/exp2.rs"]
-pub mod exp2;
-#[path = "math/exp2f.rs"]
-pub mod exp2f;
-#[path = "math/exp10.rs"]
-pub mod exp10;
-#[path = "math/exp10f.rs"]
-pub mod exp10f;
-#[path = "math/expf.rs"]
-pub mod expf;
-#[path = "math/expm1.rs"]
-pub mod expm1;
-#[path = "math/expm1f.rs"]
-pub mod expm1f;
-#[path = "math/fabs.rs"]
-pub mod fabs;
-#[path = "math/fabsf.rs"]
-pub mod fabsf;
-#[path = "math/fdim.rs"]
-pub mod fdim;
-#[path = "math/fdimf.rs"]
-pub mod fdimf;
-#[path = "math/floor.rs"]
-pub mod floor;
-#[path = "math/floorf.rs"]
-pub mod floorf;
-#[path = "math/fma.rs"]
-pub mod fma;
-#[path = "math/fmaf.rs"]
-pub mod fmaf;
-#[path = "math/fmax.rs"]
-pub mod fmax;
-#[path = "math/fmaxf.rs"]
-pub mod fmaxf;
-#[path = "math/fmin.rs"]
-pub mod fmin;
-#[path = "math/fminf.rs"]
-pub mod fminf;
-#[path = "math/fmod.rs"]
-pub mod fmod;
-#[path = "math/fmodf.rs"]
-pub mod fmodf;
-#[path = "math/frexp.rs"]
-pub mod frexp;
-#[path = "math/frexpf.rs"]
-pub mod frexpf;
-#[path = "math/hypot.rs"]
-pub mod hypot;
-#[path = "math/hypotf.rs"]
-pub mod hypotf;
-#[path = "math/ilogb.rs"]
-pub mod ilogb;
-#[path = "math/ilogbf.rs"]
-pub mod ilogbf;
-#[path = "math/j0.rs"]
-pub mod j0;
-#[path = "math/j0f.rs"]
-pub mod j0f;
-#[path = "math/j1.rs"]
-pub mod j1;
-#[path = "math/j1f.rs"]
-pub mod j1f;
-#[path = "math/jn.rs"]
-pub mod jn;
-#[path = "math/jnf.rs"]
-pub mod jnf;
-#[path = "math/ldexp.rs"]
-pub mod ldexp;
-#[path = "math/ldexpf.rs"]
-pub mod ldexpf;
-#[path = "math/lgamma.rs"]
-pub mod lgamma;
-#[path = "math/lgamma_r.rs"]
-pub mod lgamma_r;
-#[path = "math/lgammaf.rs"]
-pub mod lgammaf;
-#[path = "math/lgammaf_r.rs"]
-pub mod lgammaf_r;
-#[path = "math/log.rs"]
-pub mod log;
-#[path = "math/log1p.rs"]
-pub mod log1p;
-#[path = "math/log1pf.rs"]
-pub mod log1pf;
-#[path = "math/log2.rs"]
-pub mod log2;
-#[path = "math/log2f.rs"]
-pub mod log2f;
-#[path = "math/log10.rs"]
-pub mod log10;
-#[path = "math/log10f.rs"]
-pub mod log10f;
-#[path = "math/logf.rs"]
-pub mod logf;
-#[path = "math/modf.rs"]
-pub mod modf;
-#[path = "math/modff.rs"]
-pub mod modff;
-#[path = "math/nextafter.rs"]
-pub mod nextafter;
-#[path = "math/nextafterf.rs"]
-pub mod nextafterf;
-#[path = "math/pow.rs"]
-pub mod pow;
-#[path = "math/powf.rs"]
-pub mod powf;
-#[path = "math/remainder.rs"]
-pub mod remainder;
-#[path = "math/remainderf.rs"]
-pub mod remainderf;
-#[path = "math/remquo.rs"]
-pub mod remquo;
-#[path = "math/remquof.rs"]
-pub mod remquof;
-#[path = "math/round.rs"]
-pub mod round;
-#[path = "math/roundf.rs"]
-pub mod roundf;
-#[path = "math/scalbn.rs"]
-pub mod scalbn;
-#[path = "math/scalbnf.rs"]
-pub mod scalbnf;
-#[path = "math/sin.rs"]
-pub mod sin;
-#[path = "math/sincos.rs"]
-pub mod sincos;
-#[path = "math/sincosf.rs"]
-pub mod sincosf;
-#[path = "math/sinf.rs"]
-pub mod sinf;
-#[path = "math/sinh.rs"]
-pub mod sinh;
-#[path = "math/sinhf.rs"]
-pub mod sinhf;
-#[path = "math/sqrt.rs"]
-pub mod sqrt;
-#[path = "math/sqrtf.rs"]
-pub mod sqrtf;
-#[path = "math/tan.rs"]
-pub mod tan;
-#[path = "math/tanf.rs"]
-pub mod tanf;
-#[path = "math/tanh.rs"]
-pub mod tanh;
-#[path = "math/tanhf.rs"]
-pub mod tanhf;
-#[path = "math/tgamma.rs"]
-pub mod tgamma;
-#[path = "math/tgammaf.rs"]
-pub mod tgammaf;
-#[path = "math/trunc.rs"]
-pub mod trunc;
-#[path = "math/truncf.rs"]
-pub mod truncf;
-#[path = "math/y0.rs"]
-pub mod y0;
-#[path = "math/y0f.rs"]
-pub mod y0f;
-#[path = "math/y1.rs"]
-pub mod y1;
-#[path = "math/y1f.rs"]
-pub mod y1f;
-#[path = "math/yn.rs"]
-pub mod yn;
-#[path = "math/ynf.rs"]
-pub mod ynf;
+pub mod math {
+    pub mod acos;
+    pub mod acosf;
+    pub mod acosh;
+    pub mod acoshf;
+    pub mod asin;
+    pub mod asinf;
+    pub mod asinh;
+    pub mod asinhf;
+    pub mod atan;
+    pub mod atan2;
+    pub mod atan2f;
+    pub mod atanf;
+    pub mod atanh;
+    pub mod atanhf;
+    pub mod cbrt;
+    pub mod cbrtf;
+    pub mod ceil;
+    pub mod ceilf;
+    pub mod copysign;
+    pub mod copysignf;
+    pub mod cos;
+    pub mod cosf;
+    pub mod cosh;
+    pub mod coshf;
+    pub mod erf;
+    pub mod erfc;
+    pub mod erfcf;
+    pub mod erff;
+    pub mod exp;
+    pub mod exp2;
+    pub mod exp2f;
+    pub mod exp10;
+    pub mod exp10f;
+    pub mod expf;
+    pub mod expm1;
+    pub mod expm1f;
+    pub mod fabs;
+    pub mod fabsf;
+    pub mod fdim;
+    pub mod fdimf;
+    pub mod floor;
+    pub mod floorf;
+    pub mod fma;
+    pub mod fmaf;
+    pub mod fmax;
+    pub mod fmaxf;
+    pub mod fmin;
+    pub mod fminf;
+    pub mod fmod;
+    pub mod fmodf;
+    pub mod frexp;
+    pub mod frexpf;
+    pub mod hypot;
+    pub mod hypotf;
+    pub mod ilogb;
+    pub mod ilogbf;
+    pub mod j0;
+    pub mod j0f;
+    pub mod j1;
+    pub mod j1f;
+    pub mod jn;
+    pub mod jnf;
+    pub mod ldexp;
+    pub mod ldexpf;
+    pub mod lgamma;
+    pub mod lgamma_r;
+    pub mod lgammaf;
+    pub mod lgammaf_r;
+    pub mod log;
+    pub mod log1p;
+    pub mod log1pf;
+    pub mod log2;
+    pub mod log2f;
+    pub mod log10;
+    pub mod log10f;
+    pub mod logf;
+    pub mod modf;
+    pub mod modff;
+    pub mod nextafter;
+    pub mod nextafterf;
+    pub mod pow;
+    pub mod powf;
+    pub mod remainder;
+    pub mod remainderf;
+    pub mod remquo;
+    pub mod remquof;
+    pub mod round;
+    pub mod roundf;
+    pub mod scalbn;
+    pub mod scalbnf;
+    pub mod sin;
+    pub mod sincos;
+    pub mod sincosf;
+    pub mod sinf;
+    pub mod sinh;
+    pub mod sinhf;
+    pub mod sqrt;
+    pub mod sqrtf;
+    pub mod tan;
+    pub mod tanf;
+    pub mod tanh;
+    pub mod tanhf;
+    pub mod tgamma;
+    pub mod tgammaf;
+    pub mod trunc;
+    pub mod truncf;
+    pub mod y0;
+    pub mod y0f;
+    pub mod y1;
+    pub mod y1f;
+    pub mod yn;
+    pub mod ynf;
+}
